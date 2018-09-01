acd377d623f2c04ce29f03af2cb6ef0da545ac3d
Jimmy R <jimmylroland@gmail.com>
2018-09-01 18:51:59 -0500

    scroll picker weird behavior when adding options

--- src/assistant/App.vue
+++ src/assistant/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="top-bar">
+      <h2>top bar</h2>
+    </div>
+    <div id="main-card">
+      <AnswersInput />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnswersInput from './components/AnswersInput.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AnswersInput,
   }
 }
 </script>
 
 <style>
+#top-bar {
+  border: solid;
+  border-width: 0em 0em 0.1em 0em;
+  border-color: #e7e7e7;
+  background-color: #f6f6f6;
+  margin-bottom: 1em;
+}
+#main-card {
+  margin: auto;
+  background-color: #f6f6f6;
+  max-width: 50%;
+  min-width: 350px;
+}
+.top-card {
+  border: solid;
+  border-radius: 15px 15px 0px 0px;
+  border-color: blueviolet;
+  border-width: 0.3em;
+  color: blueviolet;
+  background-color: rgb(232, 207, 255);
+}
+.body-card {
+  border: solid;
+  border-color: blueviolet;
+  border-width: 0em 0.3em 0em 0.3em;
+}
+.bottom-card {
+  border: solid;
+  border-radius: 0px 0px 15px 15px;
+  border-color: blueviolet;
+  border-width: 0.3em;
+  color: blueviolet;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/assistant/components/AnswersInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div id="questions-label" class="top-card">
+      <h2>Questions</h2>
+    </div>
+    <div id="number-display" class="body-card">
+      <scroll-picker class="testtest" v-model="current" :options="ops"></scroll-picker>
+    </div>
+    <div @click=IncreaseOps() id="answer-selector" class="bottom-card">
+      abcd
+    </div>
+  </div>
+</template>
+
+<style>
+.testtest {
+  height: 7em !important;
+  background-color: aqua;
+}
+#number-display{
+  background-color: white;
+}
+</style>
+
+<script>
+import {ScrollPicker, ScrollPickerGroup} from 'vue-scroll-picker';
+
+export default {
+  name: "Answersinput",
+  components: {
+    ScrollPicker,
+    ScrollPickerGroup,
+  },
+  data() {
+    return {
+      current: 1,
+      ops: [1,2,3,4],
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    IncreaseOps() {
+      console.log("adding stuff");
+      this.ops.push(this.ops.length + 1);
+    },
+  }
+}
+</script>
